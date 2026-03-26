35352e632baf91c6e643fd002f15e2e3c07b2af9
KevinRudyHsieh <117444524+KevinRudyHsieh@users.noreply.github.com>
2026-03-26 01:29:45 +0100

    added factory seeders
    struggling to make migration work

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
+use App\Models\Car;
+
+class CarFactory extends Factory
+{
+    protected $model = Car::class;
+
+    public function definition(): array
+    {
+        return [
+            'license_plate' => strtoupper($this->faker->bothify('??-###-?')),
+            'brand' => $this->faker->randomElement(['Volkswagen', 'BMW', 'Audi', 'Mercedes', 'Toyota']),
+            'model' => $this->faker->word,
+            'price' => $this->faker->numberBetween(1000, 50000),
+
+            // Voeg deze velden toe om de database errors te voorkomen:
+            'mileage' => $this->faker->numberBetween(0, 300000), // Kilometerstand
+            'seats'   => $this->faker->randomElement([2, 4, 5, 7]),   // Aantal stoelen
+            'doors'   => $this->faker->randomElement([2, 3, 4, 5]),   // Aantal deuren
+            'year'    => $this->faker->numberBetween(2000, 2024),    // Bouwjaar
+
+            // De koppeling met de User
+            'user_id' => User::inRandomOrder()->first()?->id ?? User::factory(),
+        ];
+    }
+}
--- database/migrations/2023_02_17_101901_create_cars_table.php
+++ database/migrations/2023_02_17_101901_create_cars_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('brand');
             $table->string('model');
             $table->decimal('price');
-            $table->integer('mileage');
-            $table->integer('seats')->nullable();
+            $table->integer('mileage')->nullable();
+            $table->integer('seats')->default(5);
             $table->integer('doors')->nullable();
             $table->integer('production_year')->nullable();
             $table->integer('weight')->nullable();
